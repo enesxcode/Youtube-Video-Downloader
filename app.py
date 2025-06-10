85b5908756fc2ee398a6eb496635a7abd2a75d3a
Ernest Frimpong <ernest.40frimpong@gmail.com>
2025-06-10 14:14:39 -0400

    Update app.py to configure port from environment variable for deployment

--- app.py
+++ app.py
@@ -152,4 +152,6 @@ def download_file(filename):
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
